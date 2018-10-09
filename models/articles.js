76dd1eb505664ff584b7cfecc467d36ff25738ae
Sachin Puttarangaswamy <sachin.puttarangaswamy@volvo.com>
2018-10-09 11:32:05 +0530

    checkin

---
+++ models/articles.js
@@ -0,0 +1,50 @@
+export default ArticleList = [
+    {
+        "categoryId": 1,
+        "articleId": 1,
+        "articleName":"artilce name",
+        "articleAuthor":"artilce authoor",
+        "articleImageName":"artilce name",
+        "articleDescription":"artilce description",
+    }, 
+    {
+        "categoryId": 1,
+        "articleId": 2,
+        "articleName":"artilce name",
+        "articleAuthor":"artilce authoor",
+        "articleImageName":"artilce name",
+        "articleDescription":"artilce description",
+    },
+    {
+        "categoryId": 1,
+        "articleId": 3,
+        "articleName":"artilce name",
+        "articleAuthor":"artilce authoor",
+        "articleImageName":"artilce name",
+        "articleDescription":"artilce description",
+    },
+    {
+        "categoryId": 1,
+        "articleId": 4,
+        "articleName":"artilce name",
+        "articleAuthor":"artilce authoor",
+        "articleImageName":"artilce name",
+        "articleDescription":"artilce description",
+    },
+    {
+        "categoryId": 1,
+        "articleId": 5,
+        "articleName":"artilce name",
+        "articleAuthor":"artilce authoor",
+        "articleImageName":"artilce name",
+        "articleDescription":"artilce description",
+    },
+    {
+        "categoryId": 1,
+        "articleId": 1,
+        "articleName":"artilce name",
+        "articleAuthor":"artilce authoor",
+        "articleImageName":"artilce name",
+        "articleDescription":"artilce description",
+    }
+];
