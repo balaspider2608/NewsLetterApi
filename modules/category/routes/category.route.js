94236f9ceed014ae96c429eaa936ce9a3aa8e307
Nandan A <nandan.a@volvo.com>
2018-10-16 14:52:22 +0530

    created new structure for controllers

--- modules/category/routes/category.route.js
+++ modules/category/routes/category.route.js
@@ -2,8 +2,10 @@
 var categories = require('../controller/category.controller');
 
 module.exports = (app, { Category }) => {
+
+    var categoryController = require('../controller/category.controller')(Category);
     app.route('/api/Category')
         .get((req, res) => {
             res.send('Hello world');
-        }).post(categories.create);
+        }).post(categoryController.create);
 }
